719724e7b72a96d1436c89dea74f39e5971526e2
RDecreuse <renny.dcrs@gmail.com>
2016-08-10 10:25:53 +0200

    Add document id column

--- database/incremental/01_createAnimalTable.sql
+++ database/incremental/01_createAnimalTable.sql
@@ -1,5 +1,6 @@
 CREATE TABLE "animal" (
   animal_id   SERIAL PRIMARY KEY,
   name        VARCHAR(50),
+  documentId  VARCHAR(50),
   age         NUMERIC
 );
--- database/testdata/LOAD_01_animal_dataset.sql
+++ database/testdata/LOAD_01_animal_dataset.sql
@@ -1,4 +1,4 @@
-INSERT INTO animal (name, age) VALUES ('Marguerite', 1);
-INSERT INTO animal (name, age) VALUES ('Lolita', 2);
-INSERT INTO animal (name, age) VALUES ('Laracroft', 5);
-INSERT INTO animal (name, age) VALUES ('Nougatine', 3);
+INSERT INTO animal (name, documentId, age) VALUES ('Marguerite','animal_100', 1);
+INSERT INTO animal (name, documentId, age) VALUES ('Lolita','animal_101', 2);
+INSERT INTO animal (name, documentId, age) VALUES ('Laracroft','animal_102', 5);
+INSERT INTO animal (name, documentId, age) VALUES ('Nougatine','animal_103', 3);
